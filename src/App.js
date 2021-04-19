5e727f37042d347735c2f7aacd11301f0ae88b32
LzGui <lzguilhermecp@gmail.com>
2021-04-19 11:34:51 -0300

    Filtros adicionados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaRegCheckSquare } from "react-icons/fa";
+import { FaRegSquare } from "react-icons/fa";
+import { useState } from 'react';
 
 function App() {
+
+  const [list, setList] = useState([]);
+  const [listFilter, setListFilter] = useState([]);
+  const [isOnlyPending, setIsOnlyPending] = useState([]);
+  const [isEditing, setIsEditing] = useState([]);
+
+  function onSubmit(e) {
+    e.preventDefault()
+    console.log(e.target.task.value);
+
+    const task = {
+      id: new Date(),
+      name: e.target.task.value,
+      status: 'pendente'
+    };
+    setList([...list, task]);
+    setListFilter([...list, task]);
+    document.getElementById("task").value = "";
+  }
+
+  function toggle(item) {
+    const statusToUpdate = item.status === "pendente" ? "feito" : "pendente";
+    const newList = list.map((t) => {
+      if (t.id === item.id) t.status = statusToUpdate;
+      return t;
+    });
+    setList(newList);
+  }
+
+  function filter() {
+    const listToFilter = listFilter.filter(item => {
+      return !isOnlyPending ? item.status === "pendente" : true;
+    });
+    setListFilter(listToFilter);
+    setIsOnlyPending(!isOnlyPending);
+  }
+
+  function save(newName, item) {
+    const newList = list.map((t) => {
+      if (t.id === item.id) t.name = newName;
+      return t;
+    });
+    setList(newList);
+    setIsEditing("");
+  }
+
+  function onKeyDown(e, item) {
+    if (e.charCode === 13 || e.keyCode === 13)
+      save(e.target.value, item);
+  }
+
+  function onBlur(e, item) {
+    save(e.target.value, item);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <form onSubmit={onSubmit}>
+        <input name="task" />
+        <button>Adicionar</button>
+      </form>
+
+      <div>
+        <a href="#" onClick={filter}>
+          {isOnlyPending ? "Todos" : "Pendentes"}
         </a>
-      </header>
+      </div>
+      <ul>
+        {list.map((item, index) => {
+
+          return (
+            <li style={item.status === "feito" ? { textDecoration: "line-through" } : {}} key={index}>
+              <span>
+                {isEditing === item.id ?
+                  <input defaultValue={item.name} onBlur={(e) => onBlur(e, item)} onKeyDown={(e) => onKeyDown(e, item)} /> :
+                  <b onClick={() => setIsEditing(item.id)}> {item.name}</b>}
+              </span>
+              <button onClick={() => toggle(item)}>
+                {item.status === "feito" ?
+                  <FaRegCheckSquare /> :
+                  <FaRegSquare />}
+              </button>
+
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
