b1f9cbd9bea6bf8a5aab8a2a75e132c878f68f4b
LzGui <lzguilhermecp@gmail.com>
2021-04-20 11:51:01 -0300

    Rotas e API

--- src/App.js
+++ src/App.js
@@ -1,97 +1,27 @@
-import './App.css';
-import { FaRegCheckSquare } from "react-icons/fa";
-import { FaRegSquare } from "react-icons/fa";
-import { useState } from 'react';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import TaskLists from "./pages/TaskLists";
+import Tasks from "./pages/Tasks";
+import { Layout } from "antd";
+import "antd/dist/antd.css";
 
 function App() {
 
-  const [list, setList] = useState([]);
-  const [listFilter, setListFilter] = useState([]);
-  const [isOnlyPending, setIsOnlyPending] = useState([]);
-  const [isEditing, setIsEditing] = useState([]);
-
-  function onSubmit(e) {
-    e.preventDefault()
-    console.log(e.target.task.value);
-
-    const task = {
-      id: new Date(),
-      name: e.target.task.value,
-      status: 'pendente'
-    };
-    setList([...list, task]);
-    setListFilter([...list, task]);
-    document.getElementById("task").value = "";
-  }
-
-  function toggle(item) {
-    const statusToUpdate = item.status === "pendente" ? "feito" : "pendente";
-    const newList = list.map((t) => {
-      if (t.id === item.id) t.status = statusToUpdate;
-      return t;
-    });
-    setList(newList);
-  }
-
-  function filter() {
-    const listToFilter = listFilter.filter(item => {
-      return !isOnlyPending ? item.status === "pendente" : true;
-    });
-    setListFilter(listToFilter);
-    setIsOnlyPending(!isOnlyPending);
-  }
-
-  function save(newName, item) {
-    const newList = list.map((t) => {
-      if (t.id === item.id) t.name = newName;
-      return t;
-    });
-    setList(newList);
-    setIsEditing("");
-  }
-
-  function onKeyDown(e, item) {
-    if (e.charCode === 13 || e.keyCode === 13)
-      save(e.target.value, item);
-  }
-
-  function onBlur(e, item) {
-    save(e.target.value, item);
-  }
-
   return (
-    <div className="App">
-      <form onSubmit={onSubmit}>
-        <input name="task" />
-        <button>Adicionar</button>
-      </form>
+    <Layout.Content style={{ padding: 20 }}>
 
-      <div>
-        <a href="#" onClick={filter}>
-          {isOnlyPending ? "Todos" : "Pendentes"}
-        </a>
-      </div>
-      <ul>
-        {list.map((item, index) => {
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <TaskLists />
+          </Route>
 
-          return (
-            <li style={item.status === "feito" ? { textDecoration: "line-through" } : {}} key={index}>
-              <span>
-                {isEditing === item.id ?
-                  <input defaultValue={item.name} onBlur={(e) => onBlur(e, item)} onKeyDown={(e) => onKeyDown(e, item)} /> :
-                  <b onClick={() => setIsEditing(item.id)}> {item.name}</b>}
-              </span>
-              <button onClick={() => toggle(item)}>
-                {item.status === "feito" ?
-                  <FaRegCheckSquare /> :
-                  <FaRegSquare />}
-              </button>
-
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+          <Route exact path="/list/:id">
+            <Tasks />
+          </Route>
+        </Switch>
+      </Router>
+    </Layout.Content>
   );
 }
 
